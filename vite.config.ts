4995b8ecba22ed0c3a80a5416257a82df51861b8
CCC970413 <makenaichuweb@gmail.com>
2025-07-21 01:08:31 +0800

    Adjust the asses file path build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // const BASE_PATH = "weather";
 
 export default defineConfig({
   // base: `/${BASE_PATH}`,
   // define: {BASE_PATH: JSON.stringify(BASE_PATH)},
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/assets/*",
+          dest: "assets",
+        },
+      ],
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+  },
 });
